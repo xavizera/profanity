108bf04cd7364c03ad0500cba0272ab4001d3e21
xavizera <100section@gmail.com>
2025-04-21 02:02:18 -0300

    refactor

--- src/Profanity.php
+++ src/Profanity.php
@@ -4,29 +4,27 @@ namespace Xavizera\Profanity;
 
 class Profanity
 {
-    protected array $words = [];
+    protected array $wordsList = [];
 
-    protected array $languages;
+    protected string $language;
 
-    public function __construct(array $languages = ['en'])
+    public function __construct(string $language = 'en')
     {
-        $this->languages = $languages;
-        $this->loadWords();
+        $this->language = $language;
+        $this->load();
     }
 
-    public function loadWords()
+    public function load()
     {
-        foreach ($this->languages as $lang) {
-            $words = include __DIR__."/../resources/data/{$lang}.php";
-            $this->words = array_flip($words);
-        }
+        $words = include __DIR__."/../resources/data/{$this->language}.php";
+        $this->wordsList = array_flip($words);
     }
 
     public function check(string $text): bool
     {
         $words = preg_split('/\b/', strtolower($text));
         foreach ($words as $word) {
-            if (isset($this->words[$word])) {
+            if (isset($this->wordsList[$word])) {
                 return true;
             }
         }
--- tests/ProfanityTest.php
+++ tests/ProfanityTest.php
@@ -3,7 +3,7 @@
 use Xavizera\Profanity\Profanity;
 
 test('it can check for profanity', function () {
-    $profanity = new Profanity(['en']);
+    $profanity = new Profanity('en');
 
     expect($profanity->check('This text contains badword'))
         ->toBeTrue();
